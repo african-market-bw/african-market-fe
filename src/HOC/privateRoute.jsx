da1f23afff835378faabfe0fea40c4022509b7c6
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-06-25 21:24:06 +0100

    -ft-post-login -created usereProfile component -ft-dispatched actions on successfull login

--- src/HOC/privateRoute.jsx
+++ src/HOC/privateRoute.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={props => (localStorage.getItem('token') ? (
+    render={props => (localStorage.getItem('userToken') ? (
       <Component {...props} />
     ) : (<Redirect to="/" />)
     )}
-  />;
-};
+  />
+);
 
 export default PrivateRoute;
