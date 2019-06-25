f8c77f0b41bf14a958515d2b711fd9e9353bec2b
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-06-25 18:55:56 +0100

    -ft-private-route -created a custom middleware -created a user reducer function

--- src/index.jsx
+++ src/index.jsx
@@ -7,21 +7,23 @@ import {
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App';
+import userReducer from './store/reducers/usersReducer';
 import * as serviceWorker from './serviceWorker';
+import customMiddleware from './token';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(
-//   null,
-//   composeEnhancers(applyMiddleware(thunk)),
-// );
+const store = createStore(
+  userReducer,
+  composeEnhancers(applyMiddleware(thunk, customMiddleware)),
+);
 
-// const app = (
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
